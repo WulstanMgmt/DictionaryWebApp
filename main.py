4cd0c2c2aeded0af42b2d7c21902b9409ef1cf42
WulstanMGMT <localroothost@proton.me>
2024-09-17 10:39:28 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# red is not added to git
+# green is added to staging area, which are files and filepaths ready to commit
+# blue means there's uncommitted changes in file
+# commit saves changes to main file
+
+import pandas as pd
+import numpy as np
+
+pd.Series()
